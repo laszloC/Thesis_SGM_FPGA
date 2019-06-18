a87e3222875a1f7592808d6309495f2df832e608
lCiople <laszlo.ciople@gmail.com>
2019-06-18 09:43:20 +0300

    added RunResults class

---
+++ PerformanceAnalyzer/PerformanceAnalyzer/RunResults.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PerformanceAnalyzer
+{
+    class RunResults
+    {
+        string InputLeftPath { get; set; }
+
+        string InputRightPath { get; set; }
+
+        string InputGroundThruthPath { get; set; }
+
+        string ResultSWDepthMapPath { get; set; }
+
+        string ResultHWDepthMapPath { get; set; }
+
+        TimeSpan ResultSWTime { get; set; }
+
+        int ResultSWRms { get; set; }
+
+        int ResultSWBadMatches { get; set; }
+
+        TimeSpan ResultHWTime { get; set; }
+
+        int ResultHWRms { get; set; }
+
+        int ResultHWBadMatches { get; set; }
+
+        double ResultHWSpeedup { get; set; }
+    }
+}
